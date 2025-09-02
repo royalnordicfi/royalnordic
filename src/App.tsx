6cd5b678e0345e790e92b399d9f35e7ae29cbf0f
Miro Vesterinen <mirovesterinen@Miros-MacBook-Air.local>
2025-09-02 23:29:10 +0300

    🎉 Restore complete website functionality - all tour pages and routing working

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,39 @@ import Tours from './components/Tours';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import NorthernLightsTour from './components/NorthernLightsTour';
+import SnowshoeRental from './components/SnowshoeRental';
+import CustomizedTour from './components/CustomizedTour';
+import PaymentSuccess from './components/PaymentSuccess';
+import PrivacyPolicy from './components/PrivacyPolicy';
+import TermsConditions from './components/TermsConditions';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   console.log('App component rendering...'); // Debug log
   
   return (
     <Router>
+      <ScrollToTop />
       <div className="App bg-black min-h-screen">
         <Header />
         <Hero />
         
-        {/* Add back more components */}
+        {/* Main page components */}
         <Tours />
         <About />
         <Contact />
         <Footer />
+        
+        {/* Tour detail pages and routing */}
+        <Routes>
+          <Route path="/northern-lights-tour" element={<NorthernLightsTour />} />
+          <Route path="/snowshoe-rental" element={<SnowshoeRental />} />
+          <Route path="/customized-tour" element={<CustomizedTour />} />
+          <Route path="/payment-success" element={<PaymentSuccess />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-conditions" element={<TermsConditions />} />
+        </Routes>
       </div>
     </Router>
   );
