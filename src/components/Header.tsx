1ec4da69b98df3f9b3848419064e56bd7542b8b2
Royal Nordic <info@royalnordic.fi>
2025-10-14 14:22:42 +0300

    Fix header logo positioning with inline style (120px margin) and move Back button much further down (pt-48 sm:pt-52)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,7 +61,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-6">
         <div className="flex justify-between items-center h-24 pt-1">
                  {/* Logo */}
-                 <div className="flex-shrink-0 flex flex-col items-center space-y-1 ml-8">
+                 <div className="flex-shrink-0 flex flex-col items-center space-y-1" style={{marginLeft: '120px'}}>
             <img 
               src="/logo.png" 
               alt="Royal Nordic Logo" 
